e73c7ff87a7da53ca603679c706180844c694ca4
Destiny Kefas <dekeji1@gmail.com>
2022-12-06 17:24:13 +0800

    checkpoint

--- src/Components/Navigation/ProfileNav.js
+++ src/Components/Navigation/ProfileNav.js
@@ -2,17 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { View, Text } from "react-native";
 
-import HomeScreen from "../../Screens/HomeScreen";
-import SingleProductScreen from "../../Screens/SingleProductScreen";
-import ShippingScreen from "../../Screens/ShippingScreen";
-import PaymentScreen from "../../Screens/PaymentScreen";
-import PlaceOrderScreen from "../../Screens/PlaceOrderScreen";
-import LoginScreen from "../../Screens/LoginScreen";
-import RegisterScreen from "../../Screens/RegisterScreen";
-import NotVerifyScreen from "../../Screens/NotVerifyScreen";
-import AccountNav from "./AccountNav";
 import OrderScreen from "../../Screens/OrderScreen";
-import BottomNav from "./BottomNav";
 import ProfileScreen from "../../Screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
